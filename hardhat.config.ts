0ea80c5975cbf849e67592d15e6abbb57cb387b8
vfat <info@vfat.tools>
2020-11-16 13:52:01 +0000

    Added typescript hardhat-deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-ethers");
+require('hardhat-deploy');
 const {ALCHEMY_API_KEY} = require("./secret")
 
 /**
@@ -17,5 +18,11 @@ module.exports = {
   },
   paths: {
     tests: "./recover/test"
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    }
   }
 };
